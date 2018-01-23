7654b95a722f257b5af639d0ed557f3997571ea0
Bastian <basti@bastibe.de>
2018-01-23 10:39:57 +0100

    formalizes test suite

--- jbof.py
+++ jbof.py
@@ -42,7 +42,10 @@ class DataSet:
         return DataSet(directory)
 
     def __init__(self, directory):
-        self.directory = Path(directory)
+        directory = Path(directory)
+        if not directory.exists():
+            raise TypeError('DataSet directory {str(directory)} does not exist')
+        self.directory = directory
 
     @property
     def entryformat(self):
--- test.py
+++ test.py
@@ -1,23 +1,43 @@
+import pytest
+
 import jbof
 import shutil
 import numpy
 
-shutil.rmtree('tmp', ignore_errors=True)
+@pytest.fixture
+def example_data():
+    d = jbof.DataSet.create_dataset('tmp', {'kind': 'dataset'})
+    e = d.create_entry({'kind': 'entry'})
+    e.create_datum('ones', numpy.ones(10), {'kind': 'ones'})
+    e.create_datum('zeros', numpy.zeros(10), {'kind': 'zeros'})
+    e = d.create_entry({'kind': 'entry'})
+    e.create_datum('twos', numpy.ones(10)*2, {'kind': 'twos'})
+    yield d
+    shutil.rmtree('tmp')
 
-d = jbof.DataSet.create_dataset('tmp', {'foo': 'bar'})
-e = d.create_entry({'foo': 'baz1'})
-e.create_datum('test1', numpy.random.randn(10), {'foo': 'foo1'})
-e.create_datum('test2', numpy.random.randn(10), {'foo': 'foo2'})
-e = d.create_entry({'foo': 'baz2'})
-e.create_datum('test', numpy.random.randn(10), {'foo': 'foo'})
+def test_dataset(example_data):
+    with pytest.raises(TypeError):
+        d = jbof.DataSet('doesnotexist')
+    d = jbof.DataSet('tmp')
+    assert d.metadata == {'kind': 'dataset', '_entryformat': None}
 
-for entry in d.all_entries():
-    print(entry.metadata)
-    for name, data in entry.all_data():
-        print(name, data.metadata, data)
+def test_import_dataset(example_data):
+    from tmp import dataset as data
+    assert data.directory.absolute() == example_data.directory.absolute()
 
-from tmp import dataset
-for entry in dataset.all_entries():
-    print(entry.metadata)
-    for name, data in entry.all_data():
-        print(name, data.metadata, data)
+def test_entries(example_data):
+    entries = list(example_data.all_entries())
+    assert len(entries) == 2
+    for entry in entries:
+        assert entry.metadata == {'kind': 'entry'}
+
+def test_data(example_data):
+    visited_data = []
+    for entry in example_data.all_entries():
+        for name, data in entry.all_data():
+            assert numpy.all(data == {'zeros': 0, 'ones': 1, 'twos': 2}[name])
+            assert len(data.metadata) == 2
+            assert data.metadata['kind'] == name
+            assert '_filename' in data.metadata
+            visited_data.append(name)
+    assert sorted(visited_data) == ['ones', 'twos', 'zeros']
